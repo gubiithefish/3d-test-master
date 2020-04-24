974ac26cdc1cc85df5f94ba787bfec92e8621deb
gubiithefish <emil@reinert.dk>
2020-04-24 06:09:07 +0200

    Updates

---
+++ precache-manifest.fea07390707e0e2929c231b9e7221903.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c291088708d54d258dbb4bf11e72b04",
+    "url": "/3d-test-master/index.html"
+  },
+  {
+    "revision": "8e79d74d801f904dfede",
+    "url": "/3d-test-master/static/css/main.ceffa989.chunk.css"
+  },
+  {
+    "revision": "4bfac79446fb363178d5",
+    "url": "/3d-test-master/static/js/2.f4207e11.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/3d-test-master/static/js/2.f4207e11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e79d74d801f904dfede",
+    "url": "/3d-test-master/static/js/main.5c5dda38.chunk.js"
+  },
+  {
+    "revision": "57f097ce0b4b4dc2563b",
+    "url": "/3d-test-master/static/js/runtime-main.5c7cf5cd.js"
+  }
+]);
